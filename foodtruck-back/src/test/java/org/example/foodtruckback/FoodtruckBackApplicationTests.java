33657f64cb07470e9a6337194a3b5191e91245f4
minion-K <rnjsalsgus06@naver.com>
2025-11-17 11:59:26 +0900

    1117 created FoodTruck Project

---
+++ foodtruck-back/src/test/java/org/example/foodtruckback/FoodtruckBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.foodtruckback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodtruckBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
