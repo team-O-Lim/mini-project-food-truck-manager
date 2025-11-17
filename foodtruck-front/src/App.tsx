13d5ccbac00ecfc0f5fc53d185e76b4de1e8d8b4
nolang22 <rnrmfwwkd22@gmail.com>
2025-11-17 17:46:28 +0900

    1117 created-frontendfile/lee

---
+++ foodtruck-front/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      푸드 트럭
+    </>
+  )
+}
+
+export default App
