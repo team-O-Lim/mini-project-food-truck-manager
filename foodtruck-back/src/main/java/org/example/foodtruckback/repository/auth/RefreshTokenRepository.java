0398d4baf2e0f7061bfc30e8ce9b70b14a5231a9
nolang22 <rnrmfwwkd22@gmail.com>
2025-11-26 19:37:54 +0900

    1126 created back security/lee

--- foodtruck-back/src/main/java/org/example/foodtruckback/repository/auth/RefreshTokenRepository.java
+++ foodtruck-back/src/main/java/org/example/foodtruckback/repository/auth/RefreshTokenRepository.java
@@ -1,6 +1,5 @@
 package org.example.foodtruckback.repository.auth;
 
-import org.example.foodtruckback.common.constants.auth.Auth;
 import org.example.foodtruckback.entity.auth.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
