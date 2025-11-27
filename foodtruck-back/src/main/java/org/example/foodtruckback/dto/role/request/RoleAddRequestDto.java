b9fcafe086452617893d045b51e30fa344b7ba2d
nolang22 <rnrmfwwkd22@gmail.com>
2025-11-27 12:50:45 +0900

    1127 updated-back-api/lee

--- foodtruck-back/src/main/java/org/example/foodtruckback/dto/role/request/RoleAddRequestDto.java
+++ foodtruck-back/src/main/java/org/example/foodtruckback/dto/role/request/RoleAddRequestDto.java
@@ -1,5 +1,6 @@
 package org.example.foodtruckback.dto.role.request;
 
+import jakarta.validation.constraints.NotBlank;
 import org.example.foodtruckback.common.enums.RoleType;
 
 public record RoleAddRequestDto(
